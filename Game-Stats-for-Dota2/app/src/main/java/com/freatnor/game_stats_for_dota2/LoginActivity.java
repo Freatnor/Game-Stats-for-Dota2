0c083ae0f3e8f883d929ecfab96562659da31665
Freatnor <jonathan.randall.taylor@gmail.com>
2016-08-29 18:52:54 -0400

    first try

--- Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/LoginActivity.java
+++ Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.freatnor.game_stats_for_dota2;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class LoginActivity extends AppCompatActivity {
+
+    final String REALM_PARAM = "Game Stats for Dota 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        final WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final LoginActivity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url,
+                                      Bitmap favicon) {
+
+                //checks the url being loaded
+                setTitle(url);
+                Uri Url = Uri.parse(url);
+
+                if (Url.getAuthority().equals(REALM_PARAM.toLowerCase())) {
+                    // That means that authentication is finished and the url contains user's id.
+                    webView.stopLoading();
+
+                    // Extracts user id.
+                    Uri userAccountUrl = Uri.parse(Url.getQueryParameter("openid.identity"));
+                    String userId = userAccountUrl.getLastPathSegment();
+
+                    // Do whatever you want with the user's steam id
+                }
+            }
+        });
+        setContentView(webView);
+
+        // Constructing openid url request
+        String url = "https://steamcommunity.com/openid/login?" +
+                "openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&" +
+                "openid.identity=http://specs.openid.net/auth/2.0/identifier_select&" +
+                "openid.mode=checkid_setup&" +
+                "openid.ns=http://specs.openid.net/auth/2.0&" +
+                "openid.realm=https://" + REALM_PARAM + "&" +
+                "openid.return_to=https://" + REALM_PARAM + "/signin/";
+
+        webView.loadUrl(url);
+
     }
 }
+
