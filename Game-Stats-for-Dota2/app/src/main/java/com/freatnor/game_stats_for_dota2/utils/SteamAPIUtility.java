c923c1b5be5c17f03609df07480abbc105934509
Freatnor <jonathan.randall.taylor@gmail.com>
2016-09-08 17:10:52 -0400

    making utility a singleton

--- Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/utils/SteamAPIUtility.java
+++ Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/utils/SteamAPIUtility.java
@@ -1,7 +1,31 @@
 package com.freatnor.game_stats_for_dota2.utils;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+
 /**
  * Created by Jonathan Taylor on 9/8/16.
  */
 public class SteamAPIUtility  {
+
+    private RequestQueue mRequestQueue;
+    private Context mContext =
+
+    //Singleton stuff
+    private static SteamAPIUtility sInstance;
+
+    private SteamAPIUtility(Context context){
+        mContext = context.getApplicationContext();
+    }
+
+    public SteamAPIUtility getInstance(Context context){
+        if(sInstance == null){
+            sInstance = new SteamAPIUtility(context);
+        }
+        return sInstance;
+    }
+
+
+
 }
