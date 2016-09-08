2f9fd3585fcfc14bf4d216b37f2e63248bcca24d
Freatnor <jonathan.randall.taylor@gmail.com>
2016-09-07 21:44:30 -0400

    fragment on create completed

--- Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/PlayerDetailFragment.java
+++ Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/PlayerDetailFragment.java
@@ -1,7 +1,82 @@
 package com.freatnor.game_stats_for_dota2;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
 /**
  * Created by Jonathan Taylor on 9/7/16.
  */
-public class PlayerDetailFragment {
+public class PlayerDetailFragment extends Fragment{
+
+    public static final int COMPETITIVE = 10;
+    public static final int NORMAL = 20;
+
+    private int mGameTypeFlag;
+
+    //TODO change to the correct player object
+    private Player mPlayer;
+
+    private ImageView mPlayerIcon;
+
+    private Button mFollowButton;
+
+    private TextView mPlayerName;
+    private TextView mPlayerID;
+    private TextView mTotalGames;
+    private TextView mTotalTime;
+    private TextView mLastPlayed;
+    private TextView mWins;
+    private TextView mLosses;
+    private TextView mWinPercentage;
+    private TextView mKills;
+    private TextView mDeaths;
+    private TextView mAssists;
+
+    //For the future
+    private TextView mSoloMMR;
+    private TextView mPartyMMR;
+
+    public PlayerDetailFragment getInstance(Player player, int gameTypeFlag){
+        PlayerDetailFragment fragment = new PlayerDetailFragment();
+        fragment.mPlayer = player;
+        fragment.mGameTypeFlag = gameTypeFlag;
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_player_detail, container, false);
+        mPlayerIcon = (ImageView) fragmentView.findViewById(R.id.player_icon);
+
+        mFollowButton = (Button) fragmentView.findViewById(R.id.follow_button);
+
+        mPlayerName = (TextView) fragmentView.findViewById(R.id.player_name);
+        mPlayerID = (TextView) fragmentView.findViewById(R.id.steam_id);
+        mTotalGames = (TextView) fragmentView.findViewById(R.id.total_games);
+        mTotalTime = (TextView) fragmentView.findViewById(R.id.total_time);
+        mLastPlayed = (TextView) fragmentView.findViewById(R.id.last_played_time);
+        mWins = (TextView) fragmentView.findViewById(R.id.player_career_wins);
+        mLosses = (TextView) fragmentView.findViewById(R.id.player_career_losses);
+        mWinPercentage = (TextView) fragmentView.findViewById(R.id.player_career_win_percentage);
+        mKills = (TextView) fragmentView.findViewById(R.id.player_career_kills);
+        mDeaths = (TextView) fragmentView.findViewById(R.id.player_career_deaths);
+        mAssists = (TextView) fragmentView.findViewById(R.id.player_career_assists);
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //TODO bind all the data
+    }
 }
---
+++ Game-Stats-for-Dota2/app/src/main/java/com/freatnor/game_stats_for_dota2/presenters/PlayerDetailPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.freatnor.game_stats_for_dota2.presenters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Jonathan Taylor on 9/7/16.
+ */
+public class PlayerDetailPagerAdapter extends FragmentPagerAdapter {
+
+
+    public PlayerDetailPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
